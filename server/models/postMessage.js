00b8e397946694c7bf58bddfb6648c84dfdcde8c
Mohammad <mo.litkouhi@gmail.com>
2022-08-04 11:55:08 -0700

    Created Schema and turn it to the model using .Schema and .model methods.

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -1,1 +1,24 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+
+// Creating mongoose Schema
+const postSchema = mongoose.Schema({
+  title: String,
+  message: String,
+  creator: String,
+  tags: [String],
+  //  Where we convert our image to string using filed-based64
+  selectedFile: String,
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+// Turn our created Schema into a Model using .model method.
+const postMessage = mongoose.model('postMessage', postSchema);
+
+export default postMessage;
