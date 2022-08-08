99525dbc851ab70b55c0dab4b4d64fd7b752c19b
Mohammad <mo.litkouhi@gmail.com>
2022-08-07 23:50:24 -0700

    Import required material-ui components. Importing img from images folder.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+// IMPORTING ALL REQUIRED MATERIAl-UI COMPONENT FROM @MATERIAL-UI/CORE
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+// IMPORTING IMAGE
+import memories from './assets/images/memories.png';
 
 const App = () => {
   return (
-    <div>
-      <h2>App Component</h2>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        {/* USING TYPOGRAPHY COMPONENT USUALLY FOR TEXTUAL CONTENT  */}
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+    </Container>
   );
 };
 
