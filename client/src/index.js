128b64b373884fc7567a48470829101db3f521b0
Mohammad <mo.litkouhi@gmail.com>
2022-08-20 13:15:57 -0700

    Added some lines of code.

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+// Import Redux
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+import thunk from 'redux-thunk';
+import reducers from './reducers'
+
+
 import { createRoot } from 'react-dom/client';
 import App from "./App";
+
+// Setting up redux
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
+
 
 const root = createRoot( document.getElementById("root") );
 root.render( <App /> );
